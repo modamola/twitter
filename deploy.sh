442ef5866f7b5b6c142835a919b408a0f3a3989a
modamola <118681375+modamola@users.noreply.github.com>
2022-11-24 22:14:30 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,1 @@
 # this is a deployment script
-[master 042a6d3] my deployment
- Committer: EC2 Default User <ec2-user@ip-172-31-19-91.us-west-1.compute.internal>
-Your name and email address were configured automatically based
-on your username and hostname. Please check that they are accurate.
-You can suppress this message by setting them explicitly:
-
-    git config --global user.name "Your Name"
-    git config --global user.email you@example.com
-
-After doing this, you may fix the identity used for this commit with:
-
-    git commit --amend --reset-author
-
- 2 files changed, 3 insertions(+)
- create mode 100644 deploy.sh
